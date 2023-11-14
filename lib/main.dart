1f95152acb134526f73291d6e2c80764137c03c3
TDTxLE <tdtxle@gmail.com>
2023-11-14 08:50:25 -0600

    fix (app): soporte para el idioma de español

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:xpence/utils/theme/color_theme.dart';
 import 'package:timezone/data/latest.dart' as tz;
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'data/database/hive_db.dart';
 import 'data/services/notificacion_services.dart';
@@ -40,7 +41,11 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       theme: themedark,
       title: 'Xpence',
-     
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('es', 'ES')],
       routerConfig: xrouter.router,
     );
   }
